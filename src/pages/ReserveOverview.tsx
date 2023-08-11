5030ddf50079b19764ce1a8906fdc103470bf159
Lanre Akintayo <akintayolanre2019@gmail.com>
2023-08-11 08:16:48 +0100

    working on the reserve overview page responsiveness

--- src/pages/ReserveOverview.tsx
+++ src/pages/ReserveOverview.tsx
@@ -70,72 +70,76 @@ export default function ReserveOverview() {
     <div className="">
       {token ? (
         <div className="">
-          <div className="md:pt-28 bg-gradient-to-tr from-slate-900 to-gray-900 h-[320px] px-8">
-            <div className="w-full flex flex-col justify-center">
-              <div className="flex items-center">
+          <div className="py-16 md:py-28 ss:py-20  bg-gradient-to-tr from-slate-900 to-gray-900 h-[340px] ssm:h-[320px] px-8">
+            <div className="w-full flex flex-col justify-center space-y-4">
+              <div className="flex flex-col ssm:flex-row ssm:items-center space-y-3 ssm:space-y-0 ">
                 <Link
                   to="/dashboard"
-                  className="mx-2 w-[100px] h-8 bg-slate-700 flex items-center text-white rounded-md justify-center hover:bg-gray-800"
+                  className="ssm:mx-2 w-[100px] h-8 bg-slate-700 flex items-center text-white rounded-md justify-center hover:bg-gray-800"
                 >
                   <GoArrowLeft />
                   <p className="ml-2">Go back</p>
                 </Link>
 
-                <div className="flex items-center justify-center">
+                <div className="flex items-center ssm:justify-center">
                   <img
                     src={IMAGES.MATIC}
                     width={32}
                     height={28}
                     // layout="fixed"
-                    className="card-img-top"
+                    className="w-[20px] h-[20px] ssm:w-[32px] ssm:h-[28px]"
                     alt="coinimage"
                   />
-                  <p className="text-2xl text-white ml-2">Polygon Market</p>
+                  <p className="text-base ssm:text-2xl text-white ml-2">
+                    Polygon Mumbai Market
+                  </p>
                 </div>
               </div>
-              <div className="flex mt-4">
+              <div className="flex md:flex-row md:space-y-0 md:items-center flex-col items-start space-y-3">
                 <div className="flex items-center justify-center ">
                   <img
                     src={token.tokenImage}
                     width={40}
                     height={40}
-                    className="card-img-top"
+                    className="card-img-top w-[30px] h-[30px] ssm:w-[40px] ssm:h-[40px]"
                     alt="coinimage"
                   />
 
-                  <p className="text-2xl sm:text-4xl text- ml-2 text-white font-bold">
+                  <p className="text-xl ssm:text-2xl  sm:text-4xl text- ml-2 text-white font-bold">
                     {token.tokenName}
                   </p>
                 </div>
 
-                <div className="text-sm text-gray-400 ml-12">
-                  <p>Reserve Size</p>
-                  <div className="font-bold text-2xl text-white">
-                    ${fTotalSuppliedInUsd}
+                <div className="grid grid-cols-2 gap-x-8 ss:gap-x-16 sm:gap-x-0 gap-y-2 sm:flex items-center sm:space-x-8">
+                  <div className="text-[12px] ss:text-sm text-gray-400 sm:ml-12">
+                    <p>Reserve Size</p>
+                    <div className="font-bold text-base ss:text-xl sm:text-2xl text-white">
+                      ${fTotalSuppliedInUsd}
+                    </div>
                   </div>
-                </div>
-                <div className="ml-10 text-sm text-gray-400">
-                  <p>Available Liquidity</p>
-                  <div className="font-bold text-2xl text-white">
-                    ${fAvailableLiquidityInUsd}
+                  <div className="text-[12px] ss:text-sm text-gray-400">
+                    <p>Available Liquidity</p>
+                    <div className="font-bold text-base ss:text-xl sm:text-2xl text-white">
+                      ${fAvailableLiquidityInUsd}
+                    </div>
                   </div>
-                </div>
-                <div className="ml-10 text-sm text-gray-400">
-                  <p>Utilization Rate</p>
-                  <div className="font-bold text-2xl text-white">
-                    {fUtilizationRate}%
+                  <div className="text-[12px] ss:text-sm  text-gray-400">
+                    <p>Utilization Rate</p>
+                    <div className="font-bold text-base ss:text-xl sm:text-2xl text-white">
+                      {fUtilizationRate}%
+                    </div>
                   </div>
-                </div>
-                <div className="ml-10 text-sm text-gray-400">
-                  <p>Oracle price</p>
-                  <div className="font-bold text-2xl text-white">
-                    ${fOraclePrice}
+                  <div className="text-[12px] ss:text-sm  text-gray-400">
+                    <p>Oracle price</p>
+                    <div className="font-bold text-base ss:text-xl sm:text-2xl text-white">
+                      ${fOraclePrice}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="px-2 md:px-10 mx-auto w-full -mt-20">
+          <div className="px-2 md:px-10 mx-auto w-full -mt-8 sm:-mt-20">
             <div className="flex flex-wrap mt-4">
               <div className="w-full xl:w-8/12 mb-5 xl:mb-0 px-2">
                 <ReserveStatus token={token} />
